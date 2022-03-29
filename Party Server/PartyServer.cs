dd0840ac00f06133d58aa39ad9784093b53efa5e
qwerty4967 <jmcraft126@gmail.com>
2022-03-29 06:22:21 -0400

    Add files via upload

---
+++ Party Server/PartyServer.cs
@@ -0,0 +1,65 @@
+using Godot;
+using System;
+
+public class PartyServer : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+    
+    const int PORT = 9081;
+
+    WebSocketServer Server = new WebSocketServer();
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        // text
+        Server.Connect("client_connected", this, "Connected");
+        Server.Connect("client_disconnected", this, "Disconnected");
+        Server.Connect("client_close_request", this, "CloseRequest"); // we can ignore
+        Server.Connect("data_received", this, "OnData");
+        
+        // try to listen, I guess.
+        Error e = Server.Listen(PORT, null, true);
+        if (e != Error.Ok)
+        {
+            GD.Print("Unable to start server... crud.");
+           
+        }
+        else
+        {
+            GD.Print("Server up! Listening for connections at " + IP.GetLocalAddresses().ToString()+"...");
+        }
+
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        
+        Server.Poll();
+    }
+
+    public void Connected(int id, String proto)
+    {
+        GD.Print("Client " + id + " has Connected!");
+    }
+
+    public void Disconnected(int id, bool wasClean=false )
+    {
+        GD.Print("Client " + id + " Disconnected");
+
+    }
+
+    public void OnData(int id)
+    {
+        String packet = Server.GetPeer(id).GetPacket().ToString();
+
+        GD.Print("Client " + id + " Sent: " + packet);
+
+    }
+
+
+   
+}
---
+++ Party/BtnConnect.cs
@@ -0,0 +1,63 @@
+using Godot;
+using System;
+
+public class BtnConnect : Button
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    private const int PORT = 6240;
+
+    WebSocketClient client = new WebSocketClient();
+    public override void _Ready()
+    {
+        GD.Print("Hello!");
+
+        client.Connect("connection_error", this, "connectionError");
+        client.Connect("connection_established", this, "ConnectionSuccess");
+
+       
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+
+        client.Poll();
+      //  client.GetPeer(1).PutPacket("Please work".ToUTF8());
+    }
+
+    public void connectionError()
+    {
+        // oof.
+        // what was the error? who knows!
+        GD.Print("Failed to Connect.");
+    }
+
+    public void ConnectionSuccess(String protocol)
+    {
+        // send something to the server and print a message to the user.
+        GD.Print("Successfully connected!");
+        client.GetPeer(1).PutPacket("Please work".ToUTF8());
+    }
+
+    public void _on_button_up()
+    {
+        GD.Print("clicked!");
+        // wow, that was a lot
+        String IP = ((LineEdit)(GetTree().Root.GetNode("Node2D").GetNode<Node>("LineEdit"))).Text;
+
+        Error e = client.ConnectToUrl("ws://" + IP + ":9081");
+       // Error e = client.ConnectToUrl("wss://demo.piesocket.com/v3/channel_1?api_key=oCdCMcMPQpbvNjUIzqtvF1d2X2okWpDQj4AwARJuAgtjhzKxVEjQU6IdCjwm&notify_self");
+        if (e == Error.Ok)
+            return;
+        GD.PrintErr(e);
+
+        // assuming e is good
+
+       
+
+    }
+}
