4aee5739e4b93eb880aae216451b2c70e9285812
Mason S Hill <2023hillms@sa.hccsc.us>
2022-03-30 15:06:32 -0400

    slight additions to Server:

--- Party Server/PartyServer.cs
+++ Party Server/PartyServer.cs
@@ -3,64 +3,85 @@ using System;
 
 public class PartyServer : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-    
-    const int PORT = 9081;
+	// Declare member variables here. Examples:
+	// private int a = 2;
+	// private string b = "text";
+	
+	const int PORT = 9081;
 
-    WebSocketServer Server = new WebSocketServer();
+	WebSocketServer Server = new WebSocketServer();
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        // text
-        Server.Connect("client_connected", this, "Connected");
-        Server.Connect("client_disconnected", this, "Disconnected");
-        Server.Connect("client_close_request", this, "CloseRequest"); // we can ignore
-        Server.Connect("data_received", this, "OnData");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		// text
+		Server.Connect("client_connected", this, "Connected");
+		Server.Connect("client_disconnected", this, "Disconnected");
+		Server.Connect("client_close_request", this, "CloseRequest"); // we can ignore
+		Server.Connect("data_received", this, "OnData");
 
-        // try to listen, I guess.
-        Server.BindIp = "192.168.1.9";
-        Error e = Server.Listen(PORT, null, false);
-        if (e != Error.Ok)
-        {
-            GD.Print("Unable to start server: "+e);
-           
-        }
-        else
-        {
-            GD.Print("Server up! Listening for connections at " + IP.GetLocalAddresses().ToString()+"...");
-        }
+		// try to listen, I guess.
+		Server.BindIp = "172.16.112.39";
+		Error e = Server.Listen(PORT, null, false);
+		if (e != Error.Ok)
+		{
+			print("Unable to start server: "+e);
+			print("Potential IP addresses: "+IP.GetLocalAddresses().ToString());
+		   
+		}
+		else
+		{
+			print("Server up! Listening for connections at " + Server.BindIp+"...");
+		}
 
-    }
+	}
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(float delta)
-    {
-        
-        Server.Poll();
-         
-    }
+	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(float delta)
+	{
+		
+		Server.Poll();
+		 
+	}
+	
+	public void print(String toPrint)
+	{
+		GD.Print(toPrint);
+		TextEdit output = null;
+		foreach(Node child in this.GetChildren())
+		{
+			if(child.Name == "Output")
+			{
+				output = (TextEdit)child;
+			}
+		}
+		if(output==null)
+		{
+			GD.Print("Couldn't get console output");
+		}
+		
+		output.Text += toPrint+"\n";
+		
+	}
 
-    public void Connected(int id, String proto)
-    {
-        GD.Print("Client " + id + " has Connected!");
-    }
+	public void Connected(int id, String proto)
+	{
+		print("Client " + id + " has Connected!");
+	}
 
-    public void Disconnected(int id, bool wasClean=false )
-    {
-        GD.Print("Client " + id + " Disconnected");
+	public void Disconnected(int id, bool wasClean=false )
+	{
+		print("Client " + id + " Disconnected");
 
-    }
+	}
 
-    public void OnData(int id)
-    {
-        String packet = Server.GetPeer(id).GetPacket().ToString();
+	public void OnData(int id)
+	{
+		String packet = Server.GetPeer(id).GetPacket().ToString();
 
-        GD.Print("Client " + id + " Sent: " + packet);
+		print("Client " + id + " Sent: " + packet);
 
-    }
+	}
 
 
    
